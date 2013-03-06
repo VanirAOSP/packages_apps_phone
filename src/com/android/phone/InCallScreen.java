0a1124abc590a394428af7820c2c4e1f840d8e4b
Danny Baumann <dannybaumann@web.de>
2013-03-06 16:14:59 +0100

    Make going to call log after call optional.
    PS2 - Delete .xml~ file
    PS3 - For real this time
    
    Change-Id: I25a7980c8eb31bc83f67a23d72749fa3583f108f

--- src/com/android/phone/InCallScreen.java
+++ src/com/android/phone/InCallScreen.java
@@ -2769,6 +2769,7 @@ public class InCallScreen extends Activity
 
                 if ((mLastDisconnectCause != Connection.DisconnectCause.INCOMING_MISSED)
                         && (mLastDisconnectCause != Connection.DisconnectCause.INCOMING_REJECTED)
+                        && PhoneUtils.PhoneSettings.showCallLogAfterCall(this)
                         && !isPhoneStateRestricted()
                         && PhoneGlobals.sVoiceCapable) {
                     final Intent intent = mApp.createPhoneEndIntentUsingCallOrigin();
